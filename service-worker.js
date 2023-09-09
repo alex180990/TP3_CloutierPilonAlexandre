192d5a55a3ebf2fbd314a18b400bed7acc51f686
alex180990 <112338473+alex180990@users.noreply.github.com>
2023-09-09 15:08:32 -0400

    14

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,20 @@
-const CACHE_NAME = 'static-cache-v2';
+const CACHE_NAME = 'static-cache-v1.1.1';
 
 const FILES_TO_CACHE = [
-    'offline.html',
+    'index.html',
+    'guides_astuces.html',
+    'communaute_ressources.html',
 ];
 
 
 self.addEventListener('install', (evt) => {
     console.log('[ServiceWorker] Install');
-    evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-    console.log('[ServiceWorker] Pre-caching offline page');
-    return cache.addAll(FILES_TO_CACHE);
-    })
-    );
+        evt.waitUntil(
+            caches.open(CACHE_NAME).then((cache) => {
+                console.log('[ServiceWorker] Pre-caching offline page');
+                return cache.addAll(FILES_TO_CACHE);
+            })
+        );
     self.skipWaiting();
 });
 
