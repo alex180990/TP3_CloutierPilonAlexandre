0ade8e89b7ea723ac86ece0ce58d7e46fbd0093b
alex180990 <112338473+alex180990@users.noreply.github.com>
2023-09-09 16:12:44 -0400

    17

--- service-worker.js
+++ service-worker.js
@@ -43,7 +43,7 @@ self.addEventListener('fetch', (evt) => {
         .catch(() => {
             return caches.open(CACHE_NAME)
             .then((cache) => {
-            return cache.match('/' );
+            return cache.match('/TP3_CloutierPilonAlexandre/index.html' );
             });
         })
     );
