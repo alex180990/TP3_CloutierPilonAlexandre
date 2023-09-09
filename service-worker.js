6fee028135f16faf37b3674c09644a125167a1d6
alex180990 <112338473+alex180990@users.noreply.github.com>
2023-09-09 14:39:28 -0400

    12

--- service-worker.js
+++ service-worker.js
@@ -20,11 +20,32 @@ self.addEventListener('install', (evt) => {
 });
 
 self.addEventListener('activate', (evt) => {
-console.log('[ServiceWorker] Activate');
-//Remove previous cached data from disk.
-self.clients.claim();
+    console.log('[ServiceWorker] Activate');
+        evt.waitUntil(
+            caches.keys().then((keyList) => {
+                return Promise.all(keyList.map((key) => {
+                    if (key !== CACHE_NAME) {
+                        console.log('[ServiceWorker] Removing old cache',key);
+                        return caches.delete(key);
+                    }
+                }));
+            })
+        );
+    self.clients.claim();
 });
+
 self.addEventListener('fetch', (evt) => {
-console.log('[ServiceWorker] Fetch', evt.request.url);
-//Add fetch event handler here.
-});
+    console.log('[ServiceWorker] Fetch', evt.request.url);
+    if (evt.request.mode !== 'navigate') {
+        return;
+    }
+    evt.respondWith(
+        fetch(evt.request)
+        .catch(() => {
+            return caches.open(CACHE_NAME)
+            .then((cache) => {
+            return cache.match('/Cochenille/PointNClick/offline.html' );
+            });
+        })
+    );
+}); 
