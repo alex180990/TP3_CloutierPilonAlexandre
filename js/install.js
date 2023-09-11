27ebd6ecedf945f0db50a1d6965262d456d1a0a2
alex180990 <112338473+alex180990@users.noreply.github.com>
2023-09-11 19:45:16 -0400

    34

--- js/install.js
+++ js/install.js
@@ -15,11 +15,12 @@ function installPWA(evt) {
     deferredInstallPrompt.userChoice
         .then((choice) => {
             if (choice.outcome === 'accepted') {
+
                 console.log('User accepted the A2HS prompt', choice);
             } else {
                 console.log('User dismissed the A2HS prompt', choice);
             }
-            //deferredInstallPrompt = null;
+            deferredInstallPrompt = null;
         });
 }
 
