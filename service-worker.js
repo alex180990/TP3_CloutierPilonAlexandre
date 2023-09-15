bd4376b2cd4d38cb19912b519fd45c99303eb693
alex180990 <112338473+alex180990@users.noreply.github.com>
2023-09-15 19:23:14 -0400

    43

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,26 @@
-const CACHE_NAME = 'static-cache-v1.1.6';
+const CACHE_NAME = 'static-cache-v1.1.7';
 
 const FILES_TO_CACHE = [
-    'offline.html',
+    'index.html',
+    'guides_astuces.html',
+    'communaute_ressources.html',
+
+    'manifest.json',
+
+    'images/icons/icon-144x144.png',
+    'images/icons/icon-192x192.png',
+    'images/icons/icon-512x512.png',
     'images/icons/logo_titre.png',
+    'images/icons/logo.png',
+
+    'images/bgi.jpg',
+    'images/base.jpg',
+    'images/maxresdefault.jpg',
+    'images/Plateforme_de_forage.jpg',
+    'images/Rover_planetaire.jpg',
+    'images/Station_spatiale.jpg',
+    'images/Vaisseau_de_combat.jpg',
+    'images/Vaisseau_de_transport.jpg',
 ];
 
 
